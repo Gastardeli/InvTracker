ae6ae7140ba2e02c04cb62cda4844ebfd71638ca
Gustavo Pietro <gusoares.ma@gmail.com>
2025-12-01 15:02:33 -0300

    Criação do sessioStorage para pegar o id_empresa

--- siteFuncional/src/controllers/usuarioController.js
+++ siteFuncional/src/controllers/usuarioController.js
@@ -20,20 +20,28 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        estoqueModel.buscarEstoquePorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoEstoque) => {
-                                if (resultadoEstoque.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        estoque: resultadoEstoque
-                                    });
-                                } else {
-                                    res.status(204).json({ estoque: [] });
-                                }
-                            })
+
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            id_empresa: resultadoAutenticar[0].id_empresa,
+                        });
+
+                        // estoqueModel.buscarEstoquePorEmpresa(resultadoAutenticar[0].empresaId)
+                        //     .then((resultadoEstoque) => {
+                        //         if (resultadoEstoque.length > 0) {
+                        //             res.json({
+                        //                 id: resultadoAutenticar[0].id,
+                        //                 email: resultadoAutenticar[0].email,
+                        //                 nome: resultadoAutenticar[0].nome,
+                        //                 senha: resultadoAutenticar[0].senha,
+                        //                 estoque: resultadoEstoque
+                        //             });
+                        //         } else {
+                        //             res.status(204).json({ estoque: [] });
+                        //         }
+                        //     })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
