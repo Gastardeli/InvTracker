ff62c85685be1de47394784d5cd402761c476145
Gastardeli <victorgastardeli11@gmail.com>
2025-12-01 18:51:16 -0300

    Select estoque defasado

--- siteFuncional/src/models/dashDiariaModel.js
+++ siteFuncional/src/models/dashDiariaModel.js
@@ -38,3 +38,29 @@ WHERE
         AND empresa.id = 1; -- Obs no lugar de 1 deve ir o ${empresa_id} na qual o gustavo criou
 -- Obs2: essa parte  - r.distancia > (e.tamanho * 0.80) -  mostra a distância captada pelo sensor cujo tamanho é maior q 80 o tamanho total do estoque 
 */
+
+/* SELECT DO ESTOQUE COM MAIOR NECESSIDADE DE REPOSIÇÃO */
+/*
+    SELECT 
+    e.idEstoque,
+    e.estoque AS nomeEstoque,
+    emp.razao AS empresa,
+    e.tamanho AS capacidadeTotal,
+    r.distancia AS espacoVazio,
+    (e.tamanho - r.distancia) AS espacoOcupado,
+    ROUND((r.distancia / e.tamanho) * 100, 2) AS porcentagemVazio
+FROM lote l
+JOIN estoque e ON e.idEstoque = l.fkEstoque
+JOIN empresa emp ON emp.id = e.fkEmpresa
+JOIN sensor s ON s.idSensor = l.fkSensor
+JOIN (
+    SELECT fkSensor, MAX(dtRegistro) AS ultimoRegistro
+    FROM registro
+    GROUP BY fkSensor
+) last_r ON last_r.fkSensor = s.idSensor
+JOIN registro r ON r.fkSensor = s.idSensor AND r.dtRegistro = last_r.ultimoRegistro
+ORDER BY r.distancia DESC  
+LIMIT 1; 
+*/
+/* OBS, AO USER ESSE SELECT NO FETCH, VC IRÁ COLOCAR NO INNERHTML APENAS O ' e.idEstoque' EXEMPLO : ESTOQUE
+                                                                                                       X  */
