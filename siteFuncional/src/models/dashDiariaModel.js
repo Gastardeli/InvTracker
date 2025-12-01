9baf3bfa01e87706aaef9b527ecb5f3fcfab4a09
Gastardeli <victorgastardeli11@gmail.com>
2025-12-01 18:37:48 -0300

    Select dashDiária reposição

--- siteFuncional/src/models/dashDiariaModel.js
+++ siteFuncional/src/models/dashDiariaModel.js
@@ -11,3 +11,30 @@ function dashDiarialistar() {
 module.exports = {
     dashDiarialistar
 };
+
+/* SELECT DE QUANTOS LOTES PRECISAM DE REPOSIÇÃO */
+/*
+    SELECT 
+    COUNT(DISTINCT l.fkEstoque) AS EstoquesQuePrecisamReposicao
+FROM
+    lote l
+        JOIN
+    estoque e ON e.idEstoque = l.fkEstoque
+        JOIN
+    empresa ON empresa.id = e.fkEmpresa
+        JOIN
+    sensor s ON s.idSensor = l.fkSensor
+        JOIN
+    (SELECT 
+        fkSensor, MAX(dtRegistro) AS ultimoRegistro
+    FROM
+        registro
+    GROUP BY fkSensor) ult ON ult.fkSensor = s.idSensor
+        JOIN
+    registro r ON r.fkSensor = s.idSensor
+        AND r.dtRegistro = ult.ultimoRegistro
+WHERE
+    r.distancia > (e.tamanho * 0.80)
+        AND empresa.id = 1; -- Obs no lugar de 1 deve ir o ${empresa_id} na qual o gustavo criou
+-- Obs2: essa parte  - r.distancia > (e.tamanho * 0.80) -  mostra a distância captada pelo sensor cujo tamanho é maior q 80 o tamanho total do estoque 
+*/
